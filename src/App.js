7900aafb72678f2f0975dd8c2b1c5763fd84a840
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-23 11:54:38 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { GiSharpSmile } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <h1>Your Kanji</h1>
+      <h1><GiSharpSmile /></h1>
     </div>
   );
 }
